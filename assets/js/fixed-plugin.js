33949a7ecb72d15d919ebe918a0c1698eae97ea7
Jose Ferreira <jferreira.5655@unimar.edu.ve>
2024-11-21 16:02:59 -0400

    casi listo modal de registro

--- assets/js/fixed-plugin.js
+++ assets/js/fixed-plugin.js
@@ -264,21 +264,21 @@ if (navbar) {
     if (this.checked) {
       navbar.setAttribute("navbar-scroll", "true");
       navbar.classList.add("sticky");
-      navbar.classList.add("top-[1%]");
+      navbar.classList.add("top-0");
       navbar.classList.add("backdrop-saturate-[200%]");
       navbar.classList.add("backdrop-blur-[30px]");
       navbar.classList.add("bg-[hsla(0,0%,100%,0.8)]");
       navbar.classList.add("shadow-blur");
-      navbar.classList.add("z-110");
+      navbar.classList.add("z-50");
     } else {
       navbar.setAttribute("navbar-scroll", "false");
       navbar.classList.remove("sticky");
-      navbar.classList.remove("top-[1%]");
+      navbar.classList.remove("top-0");
       navbar.classList.remove("backdrop-saturate-[200%]");
       navbar.classList.remove("backdrop-blur-[30px]");
       navbar.classList.remove("bg-[hsla(0,0%,100%,0.8)]");
       navbar.classList.remove("shadow-blur");
-      navbar.classList.remove("z-110");
+      navbar.classList.remove("z-50");
     }
   });
 } else {
